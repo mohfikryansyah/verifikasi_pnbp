c557fee48779037c7704099e60c90f08fc16c184
mohfikryansyah <moh.fikryansyah@gmail.com>
2023-10-03 13:39:21 +0800

    build css

--- resources/views/auth/partials/footer.blade.php
+++ resources/views/auth/partials/footer.blade.php
@@ -1,5 +1,5 @@
 {{-- <script src="assets/js/script.js"></script> --}}
-{{-- <script src="{{ asset('build/assets/app-c18d6a5a.js') }}"></script> --}}
+<script src="{{ asset('build/assets/app-c18d6a5a.js') }}"></script>
 @livewireStyles
 </body>
 </html>
